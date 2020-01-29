f4a2df9455d56ae7d37cd81a49e9e19cca41b873
fanxiule <x54fan@edu.uwaterloo.ca>
2020-01-29 12:17:21 +0800

    Updated distance and yaw calculations

--- turtlebot_example/src/turtlebot_square.cpp
+++ turtlebot_example/src/turtlebot_square.cpp
@@ -21,6 +21,7 @@ bool isTurning = false;
 double initial_yaw;
 double yaw_target;
 double initial_x;
+double initial_y;
 
 //Callback function for the Position topic
 //void pose_callback(const geometry_msgs::PoseWithCovarianceStamped::ConstPtr& msg)
@@ -62,6 +63,7 @@ int main(int argc, char **argv)
     loop_rate.sleep();       //Maintain the loop rate
     ros::spinOnce();         //Check for new messages
     initial_x = X;
+    initial_y = Y;
     initial_yaw = Yaw;
 
     while (ros::ok())
@@ -69,35 +71,48 @@ int main(int argc, char **argv)
         loop_rate.sleep(); //Maintain the loop rate
         ros::spinOnce();   //Check for new messages
         ROS_INFO_NAMED("turtlebot", "X: %f", X);
+        ROS_INFO_NAMED("turtlebot", "Y: %f", Y);
         ROS_INFO_NAMED("turtlebot", "Initial X: %f", initial_x);
         ROS_INFO_NAMED("turtlebot", "Yaw: %f", Yaw);
         //Main loop code goes here:
         if (isTurning)
         {
-            ROS_INFO_NAMED("turtlebot", "Turning");
+            ROS_INFO_NAMED("turtlebot", "Yaw target: %f", yaw_target);
             vel.linear.x = 0;
             vel.angular.z = 0.2;
-            if (yaw_target > M_PI)
-            {
-                yaw_target -= 2 * M_PI;
-            }
+            //if (yaw_target > M_PI)
+            //{
+            //    yaw_target -= 2 * M_PI;
+            //}
 
-            if (Yaw > yaw_target)
+            if (fabs(Yaw - yaw_target) < 0.05)
             {
                 isTurning = false;
                 initial_x = X;
+                initial_y = Y;
             }
         }
         if (!isTurning)
         {
             vel.linear.x = 0.1;
             vel.angular.z = 0;
-            ROS_INFO_STREAM("abs(X - initial_x)=" << fabs(X - initial_x));
-            if (fabs(X - initial_x) > 0.1)
+            double distance = sqrt((X - initial_x) * (X - initial_x) + (Y - initial_y) * (Y - initial_y));
+            ROS_INFO_NAMED("turtlebot", "Distance travelled: %f", distance);
+            if (distance > 0.4)
             {
                 isTurning = true;
                 initial_yaw = Yaw;
                 yaw_target = M_PI / 2 + initial_yaw;
+
+                if (yaw_target > M_PI)
+                {
+                    yaw_target = -(2 * M_PI - yaw_target);
+                }
+
+                if (yaw_target < -M_PI)
+                {
+                    yaw_target = 2 * M_PI + yaw_target;
+                }
                 ROS_INFO_NAMED("turtlebot", "Change to turning");
             }
         }
