a74113feea3fa6e6693d8cf7b68079995d87d3dc
Xiule Fan <tgbakosl@edu.uwaterloo.ca>
2020-03-13 21:04:51 -0400

    backup code with a map appearing

--- turtlebot_lab2_python/turtlebot/nodes/occupancy_grid.py
+++ turtlebot_lab2_python/turtlebot/nodes/occupancy_grid.py
@@ -9,11 +9,19 @@ from nav_msgs.msg import OccupancyGrid, MapMetaData
 from geometry_msgs.msg import PoseWithCovarianceStamped, Pose
 from sensor_msgs.msg import LaserScan
 
+def prob_to_logit(prob):
+	if prob <= 0.0:
+		return -float('inf')
+	if prob == 1.0:
+		return float('inf')
+	else:
+		return log(prob/(1.0-prob))
+
 #some constants
 O_0 = 0.5
-O_occ = 1
-O_free = 0
-l_0 = log(O_0/(1-O_0))
+O_occ = 0.9
+O_free = 0.1
+l_0 = prob_to_logit(O_0)
 
 
 class mapMaker():
@@ -22,14 +30,14 @@ class mapMaker():
 		rospy.init_node('turtlebot', anonymous=True)
 
 		# initialize a publisher for occupancy grid
-		self.occupancy_pub = rospy.Publisher('/map', OccupancyGrid, queue_size=1)
-		rospy.loginfo("got into initializaytion")
+		self.occupancy_pub = rospy.Publisher('/map', OccupancyGrid, queue_size=10)
+		# rospy.loginfo("got into initializaytion")
 		self.occupancy_grid = OccupancyGrid()
 		metadata = MapMetaData()
-		metadata.map_load_time = None
-		metadata.resolution = resolution = rospy.get_param("~grid_resolution", .10)
-		metadata.width = int(rospy.get_param("~grid_width", 1000))
-		metadata.height = int(rospy.get_param("~grid_height", 1000))
+		# metadata.map_load_time = None
+		metadata.resolution = resolution = rospy.get_param("~grid_resolution", 1)
+		metadata.width = 250
+		metadata.height = 250
 		pos = np.array([-metadata.width * resolution / 2, -metadata.height * resolution / 2, 0])
 		metadata.origin = Pose()
 		metadata.origin.position.x, metadata.origin.position.y = pos[:2]
@@ -39,7 +47,7 @@ class mapMaker():
 
 		self.map_metadata = metadata
 		self.occupancy_grid.info = self.map_metadata
-		self.occupancy_grid.data = np.ones(metadata.height * metadata.width)
+		self.occupancy_grid.data = np.ones(metadata.height * metadata.width).astype(int)*O_0
 		self.laser_data = None
 		self.position = None
 		self.orientation = None
@@ -51,29 +59,29 @@ class mapMaker():
 		# initialize a subscriber to receiver estimated posiition from the kalman filter output
 		rospy.Subscriber('/indoor_pos', PoseWithCovarianceStamped, self.IPS_callback)
 
-		rate = rospy.Rate(20)  # hz
+		# rate = rospy.Rate(20)  # hz
 
 		# while ROS is still running
-		while not rospy.is_shutdown():
+		# while not rospy.is_shutdown():
 
-			"""
-			# movement forward (X-axis only)
-			msg.linear.x = 0.1
-			msg.linear.y = 0
-			msg.linear.z = 0
+		"""
+		# movement forward (X-axis only)
+		msg.linear.x = 0.1
+		msg.linear.y = 0
+		msg.linear.z = 0
 
-			# and angular rotation
-			msg.angular.x = 0
-			msg.angular.y = 0
-			msg.angular.z = 0.3
-			"""
-			
-			#pub.publish(msg)
-			rate.sleep()
+		# and angular rotation
+		msg.angular.x = 0
+		msg.angular.y = 0
+		msg.angular.z = 0.3
+		"""
+		
+		#pub.publish(msg)
+		# rate.sleep()
 
 
 	def laser_callback(self, msg):
-		#rospy.loginfo("got into laser callback")
+		rospy.loginfo("got into laser callback")
 		self.laser_data = msg
 		angle = self.laser_data.angle_min
 		self.angles = []																														
@@ -100,6 +108,7 @@ class mapMaker():
 
 
 	def update (self) :
+		rospy.loginfo("In the update")
 		#rospy.loginfo("position %s" % self.position)
 		if self.position is None:
 			return
@@ -115,14 +124,20 @@ class mapMaker():
 		for j in range(0,len(self.angles)):
 			r = self.ranges[j]
 			a = self.angles[j]
-			#rospy.loginfo("x = %s, y = %s, yaw = %s, a = %s, r =%s" % (x,y,yaw,a,r))
+			
 			cx, cy, pr = self.inverseScanner(x, y, yaw, a, r)
+			# rospy.loginfo("x = %s, y = %s, yaw = %s, a = %s, r =%s" % (x,y,yaw,a,r))
+			
 			#rospy.loginfo("cx = %s, cy = %s, pr = %s" % (cx,cy,pr))
 			for i in range(0,len(cx)):
 				idx = int(cx[i])*self.occupancy_grid.info.width + int(cy[i])
-				#rospy.loginfo("idx %s", idx)
-				#self.occupancy_grid.data[idx] = int(self.occupancy_grid.data[idx] + log(pr[i]/(1-pr[i])) - l_0)
+				#rospy.loginfo("pr %s", pr)
+				self.occupancy_grid.data[idx] = int(self.occupancy_grid.data[idx] + prob_to_logit(pr[i]) - l_0)
 		self.publish_data()
+
+
+
+
 
 	def inverseScanner(self,x_robot,y_robot,theta_robot,theta_scan,range_scan):
 		#convert scans to cartesian coordinates of end points
@@ -132,21 +147,21 @@ class mapMaker():
 		#return cx, cy, pr (lists most likely)
 		#rospy.loginfo("Tomas DEBUG x_robot = %s, y_robot = %s, theta_robot= %s, theta_scan = %s, range_scan =%s" % (x_robot,y_robot,theta_robot,theta_scan,range_scan))
 		x_obj = x_robot + range_scan * sin(theta_robot + theta_scan)
-		rospy.loginfo("test")
-		rospy.loginfo(x_obj)
+		# rospy.loginfo("test")
+		# rospy.loginfo(x_obj)
 		y_obj = y_robot + range_scan * cos(theta_robot + theta_scan)
 		x_array, y_array = self.bresenham(x_robot*self.conversion_m_to_xy, y_robot*self.conversion_m_to_xy, x_obj*self.conversion_m_to_xy, y_obj*self.conversion_m_to_xy)
-		rospy.loginfo('x_array = %s' % x_array)
-		pr = np.ones(len(x_array))*10
-		pr[-1] = 1
+		# rospy.loginfo('x_array = %s' % x_array)
+		pr = np.ones(len(x_array))*O_free
+		pr[-1] = O_occ
 		return x_array , y_array, pr
 
 	def publish_data(self):
-		occGrid = OccupancyGrid()
-		#occGrid.header.frame_id = '/map'
-		occGrid.info = self.map_metadata
-		occGrid.data = self.occupancy_grid.data
-		rospy.loginfo("about to publish")
+		data_ = [np.int8(i*100) for i in self.occupancy_grid.data]
+		occGrid = OccupancyGrid(info=self.map_metadata, data =data_)
+
+		rospy.loginfo("about to publish: %s " % len(data_))
+	
 		self.occupancy_pub.publish(occGrid) 
 
 
